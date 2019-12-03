dbedd9b847d5f871e0c05d45ed2ee083e6bdaf60
Sun Jae Lee <ssmine0104@gmail.com>
2019-12-02 20:09:08 -0500

    added hidden layer extraction script

--- YOLO_test_hiddenlayer.py
+++ YOLO_test_hiddenlayer.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append("./YOLO/")
-
+import pickle
 import torch
 import cv2
 import matplotlib.pyplot as plt
@@ -17,9 +17,38 @@ m.load_weights(weight_file)
 class_names = load_class_names(namesfile)
 plt.rcParams['figure.figsize'] = [24.0, 14.0]
 img = np.array(Image.open('./Flickr8k/Flicker8k_Dataset/2513260012_03d33305cf.jpg'))
-original_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-resized_image = cv2.resize(original_image, (m.width, m.height))
-nms_thresh = 0.6
-iou_thresh = 0.4
-hidden = get_hidden_layer(m, resized_image, iou_thresh, nms_thresh)
-print(hidden)
+img_to_hidden = {}
+
+train_images_file = './Flickr8k/Flickr8k_text/Flickr_8k.trainImages.txt'
+test_images_file = './Flickr8k/Flickr8k_text/Flickr_8k.testImages.txt'
+val_images_file = './Flickr8k/Flickr8k_text/Flickr_8k.devImages.txt'
+
+
+train_img = list(open(train_images_file, 'r').read().strip().split('\n'))
+test_img = list(open(test_images_file, 'r').read().strip().split('\n'))
+val_img = list(open(val_images_file, 'r').read().strip().split('\n'))
+filenames = ["train", "val", "test"]
+img_sets = [train_img, val_img, test_img]
+img_dicts = [{}, {}, {}]
+
+log = open("logfile.txt", "w")
+
+for i in range(3):
+    img_set = img_sets[i]
+    log.write("------Beginning "+filenames[i]+"--------\n")
+    log.flush()
+    total = len(img_set)
+    for idx, image_name in enumerate(img_set):
+        img = np.array(Image.open("./Flickr8k/Flicker8k_Dataset/" + image_name))
+        original_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        resized_image = cv2.resize(original_image, (m.width, m.height))
+        nms_thresh = 0.6
+        iou_thresh = 0.4
+        hidden = get_hidden_layer(m, resized_image, iou_thresh, nms_thresh).detach().cpu().numpy()[0]
+        img_dicts[i][image_name] = hidden
+        if idx % 100 == 0:
+            log.write("\t" + str(idx / total) + "\n")
+            log.flush()
+
+    with open(filenames[i]+"_hidden.pickle", 'wb') as handle:
+        pickle.dump(img_dicts[i], handle, protocol=pickle.HIGHEST_PROTOCOL)
